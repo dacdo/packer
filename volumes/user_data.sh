98551b44b22bd9e4023d25abdaca1a0345061732
DacDo <mr.dacdo@gmail.com>
2021-09-27 13:50:28 +1000

    updated

--- volumes/user_data.sh
+++ volumes/user_data.sh
@@ -1,5 +1,2 @@
 #! /bin/bash
-sudo mkfs -t xfs /dev/xvdb
-sudo mkdir /mxlocal
-sudo mount /dev/xvdb /mxlocal
-sudo echo testing_user_data > /tmp/dac_user_data.txt
+echo testing_user_data > /tmp/dac_user_data.txt
